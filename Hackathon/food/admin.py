f00b737c07c7a4d0f4529990e25261b49a1da6bb
HarshilShrivastava <Shrivastavahharshil12@gmail.com>
2020-06-05 12:10:14 +0530

    added database

--- Hackathon/food/admin.py
+++ Hackathon/food/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from food.models import (
-    Food
+    Food,
+    FoodNutrition,
+    Vitamins,
+    AvailablityZone,
+    Problem,
+    Processing,
+    AvailablityLevel,
+    Category,
+    FoodGroup
 )
 from import_export.admin import ImportExportModelAdmin
 
@@ -12,4 +20,39 @@ class PersonAdmin(ImportExportModelAdmin):
     search_fields = ('name', )
 
     pass
-# Register your models here.
+
+
+@admin.register(FoodNutrition)
+class PersonAdmin(ImportExportModelAdmin):
+    list_filter = ('Vitamin','Availablity','Problems_Can_Solve','Processing_level' ,'AvailablityTier','Category',)
+    search_fields = ('name', )
+    pass
+
+
+@admin.register(Vitamins)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(AvailablityZone)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Problem)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Processing)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(AvailablityLevel)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Category)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(FoodGroup)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
--- Hackathon/food/models.py
+++ Hackathon/food/models.py
@@ -16,3 +16,51 @@ class Food(models.Model):
     Image=models.ImageField(upload_to='Food/',null=True,blank=True)
     def __str__(self):
         return self.name
+
+class FoodGroup(models.Model):
+    name=models.CharField(max_length=255)
+    
+
+class Category(models.Model):
+    name=models.CharField(max_length=255)
+
+class AvailablityLevel(models.Model):
+    name=models.CharField(max_length=255)
+
+class Processing(models.Model):
+    Level=models.CharField(max_length=255)
+
+
+class Problem(models.Model):
+    name=models.CharField(max_length=255)
+
+class AvailablityZone(models.Model):
+    name=models.CharField(max_length=255)
+
+class Vitamins(models.Model):
+    name=models.CharField(max_length=255)
+
+class FoodNutrition(models.Model):
+    name=models.CharField(max_length=255)
+    description=models.TextField()
+    Food_Group = models.ForeignKey(FoodGroup, on_delete=models.CASCADE)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    AvailablityTier = models.ForeignKey(AvailablityLevel, on_delete=models.CASCADE)
+    Processing_level=models.ForeignKey(Processing,on_delete=models.CASCADE)
+    Unitconversion=models.DecimalField(decimal_places=2, null=True, blank=True,max_digits=4)
+    Calories=models.PositiveIntegerField(null=True)
+    Lactose_Intolerance=models.BooleanField(default=False)
+    Fat=models.DecimalField(decimal_places=2, null=True, blank=True,max_digits=10)
+    Protein =models.DecimalField(decimal_places=2, null=True, blank=True,max_digits=10)
+    Carbohydrate =models.DecimalField(decimal_places=2, null=True, blank=True,max_digits=10)
+    Vitamin=models.ForeignKey(Vitamins,on_delete= models.CASCADE)
+    Sugars =models.DecimalField(decimal_places=2, null=True, blank=True,max_digits=10)
+    Fiber =models.DecimalField(decimal_places=2, null=True, blank=True,max_digits=10)
+    Cholesterol =models.DecimalField(decimal_places=2, null=True, blank=True,max_digits=10)
+    Saturated_Fats =models.DecimalField(decimal_places=2, null=True, blank=True,max_digits=10)
+    Image=models.ImageField(upload_to='Food/',null=True,blank=True)
+    Problems_Can_Solve=models.ManyToManyField(Problem)
+
+    Availablity=models.ManyToManyField(AvailablityZone)
+    def __str__(self):
+        return self.name
